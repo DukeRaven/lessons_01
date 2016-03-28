632895f705629b9117c63b734731f6eef3dc640b
Duke_Raven <karamamburu@gmail.com>
2016-03-28 22:12:59 +0300

    Added repo to Idea Started second part of homework

--- lesson_02/src/SecondPart.java
+++ lesson_02/src/SecondPart.java
@@ -4,9 +4,11 @@
 public class SecondPart {
 
     public static void main(String[] args) {
-        int i = 4;
-        i /=2;
-        System.out.println(i);
+        System.out.println("Это калькулятор Java");
+        while (true) {
+            System.out.print("Введите операцию(+,-,,/) и 2 цифры(y-для выхода):");
+
+        }
 /*
         System.out.println("Это калькулятор Java");
         while (true) {
